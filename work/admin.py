d16763d00f5f7959257ed08e020e72beceb586b3
zedmk2 <sidneykann@gmail.com>
2018-02-23 08:29:32 -0500

    v1.1, minor updates

--- work/admin.py
+++ work/admin.py
@@ -3,13 +3,22 @@ from . import models
 
 # Register your models here.
 
+class JobAdmin(admin.ModelAdmin):
+    search_fields = ['job_location']
+
+class PropertyAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+
+class ClientAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+
 class JobInlineAdmin(admin.TabularInline):
     model = models.Job
     ordering = ("-job_location",)
     extra = 8
 
 class Shift_Inline_Admin(admin.ModelAdmin):
-    
+
     inlines = [
     JobInlineAdmin,
     ]
@@ -19,8 +28,8 @@ class Shift_Inline_Admin(admin.ModelAdmin):
             kwargs["queryset"] = School.objects.order_by('job_location')
         return super(Shift_Inline_Admin, self).formfield_for_foreignkey(db_field, request, **kwargs)
 
-admin.site.register(models.Job)
-admin.site.register(models.Client)
-admin.site.register(models.Property)
+admin.site.register(models.Job, JobAdmin)
+admin.site.register(models.Client, ClientAdmin)
+admin.site.register(models.Property, PropertyAdmin)
 admin.site.register(models.Employee)
 admin.site.register(models.Shift,Shift_Inline_Admin)
