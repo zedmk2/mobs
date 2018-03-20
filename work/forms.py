f5d2a6cff4cd7cc12a17e096f1208d25a02c7042
zedmk2 <sidneykann@gmail.com>
2018-03-20 12:17:52 -0400

    small edits

--- work/forms.py
+++ work/forms.py
@@ -37,8 +37,8 @@ class CreateShiftForm(forms.ModelForm):
     he_2_start_time = forms.TimeField(widget=forms.TextInput(attrs={'class': 'special','type':'time'}))
     he_2_end_time = forms.TimeField(widget=forms.TextInput(attrs={'class': 'special','type':'time'}))
     driver = forms.ModelChoiceField(queryset = models.Employee.objects.filter(driver=True))
-    helper = forms.ModelChoiceField(queryset = models.Employee.objects.filter(helper=True))
-    helper_2 = forms.ModelChoiceField(queryset = models.Employee.objects.filter(helper=True))
+    helper = forms.ModelChoiceField(queryset = models.Employee.objects.filter(helper=True),required=False)
+    helper_2 = forms.ModelChoiceField(queryset = models.Employee.objects.filter(helper=True),required=False)
     #Update to ModelChoice/Queryset when equipment model added
     trucks = [(415,'415'),(501,'501'),(502,'502'),(503,'503'),(504,'504')]
     truck = forms.ChoiceField(choices = trucks)
